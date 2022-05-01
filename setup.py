9b9fc45d9e23d63b5a09609b6c6d518f4d07c4a3
Tom Gillespie <tgbugs@gmail.com>
2022-04-30 22:12:45 -0700

    setup.py add sxpr optional deps, update classifiers

--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@ from setuptools import setup
 
 
 def find_version(filename):
-    _version_re = re.compile(r"__version__ = '(.*)'")
+    _version_re = re.compile(r"__version__ = ['\"](.*)['\"]")
+    last = None  # match python semantics
     for line in open(filename):
         version_match = _version_re.match(line)
         if version_match:
-            return version_match.group(1)
+            last = version_match.group(1)
+
+    return last
 
 
 __version__ = find_version('orthauth/__init__.py')
@@ -16,7 +19,8 @@ with open('README.md', 'rt') as f:
     long_description = f.read()
 
 yaml_requires = ['pyyaml']
-tests_require = ['pytest', 'pytest-runner'] + yaml_requires
+sxpr_requires = ['sxpyr']
+tests_require = ['pytest', 'pytest-runner'] + yaml_requires + sxpr_requires
 setup(name='orthauth',
       version=__version__,
       description='A library to separate configuration and authentication from program logic',
@@ -32,6 +36,11 @@ setup(name='orthauth',
           'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10',
+          'Programming Language :: Python :: 3.11',
+          'Programming Language :: Python :: Implementation :: CPython',
+          'Programming Language :: Python :: Implementation :: PyPy',
           'Operating System :: POSIX :: Linux',
           'Operating System :: MacOS :: MacOS X',
           'Operating System :: Microsoft :: Windows',
@@ -46,6 +55,7 @@ setup(name='orthauth',
       install_requires=[],
       extras_require={'test': tests_require,
                       'yaml': yaml_requires,
+                      'sxpr': sxpr_requires,
                      },
       scripts=[],
       entry_points={'console_scripts': [ ],},
